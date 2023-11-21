0ef0482f2ab4739d0d65a101fe96b42ee94a3f73
tupaerp <134621789+tupaerp@users.noreply.github.com>
2023-11-21 09:09:51 -0300

    Update PrecisoPRODbContext.cs

--- PrecisoPRO/Data/PrecisoPRODbContext.cs
+++ PrecisoPRO/Data/PrecisoPRODbContext.cs
@@ -6,7 +6,8 @@ namespace PrecisoPRO.Data
     public class PrecisoPRODbContext : DbContext
     {
         public PrecisoPRODbContext(DbContextOptions<PrecisoPRODbContext> options) : base(options) { }
-
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Empresa> Empresas { get; set; }
 
 
 
