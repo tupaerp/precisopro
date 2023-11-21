7a50aa69bc6326e2eaf71604948b0a2fa8be3206
tupaerp <tupaerp@gmail.com>
2023-11-21 08:37:45 -0300

    Atualização layout e login index

--- PrecisoPRO/Data/PrecisoPRODbContext.cs
+++ PrecisoPRO/Data/PrecisoPRODbContext.cs
@@ -6,5 +6,9 @@ namespace PrecisoPRO.Data
     public class PrecisoPRODbContext : DbContext
     {
         public PrecisoPRODbContext(DbContextOptions<PrecisoPRODbContext> options) : base(options) { }
+
+
+
+
     }
 }
