9442f658074de0d70b4c3c1472f82f1ffec4b57b
tupaerp <tupaerp@gmail.com>
2023-11-29 18:10:03 -0300

    com as alteracoes do matheus

---
+++ PrecisoPRO/wwwroot/lib/bootstrap/dist/js/sidebar.js
@@ -0,0 +1,36 @@
+﻿const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text"),
+    modeImg = body.querySelector(".mode-img");
+
+
+
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+
+})
+
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+
+})
+  
+
+
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Modo Claro";
+        modeImg.innerHTML = '<img src="lib/IMG/LOGOMENOR2.png" width="190" />';
+        
+    } else {
+        modeText.innerText = "Modo Escuro";
+        modeImg.innerHTML = '<img src="lib/IMG/LOGOMENOR.png" width="190" />';
+
+    }
+});
