21035cc6663b2df040d59cedd805eda3919138a5
tupaerp <tupaerp@gmail.com>
2023-12-04 10:09:39 -0300

    CriacaoUsuario

--- PrecisoPRO/Repository/UsuarioRepository.cs
+++ PrecisoPRO/Repository/UsuarioRepository.cs
@@ -1,6 +1,65 @@
-﻿namespace PrecisoPRO.Repository
+﻿using Microsoft.EntityFrameworkCore;
+using PrecisoPRO.Data;
+using PrecisoPRO.Interfaces;
+using PrecisoPRO.Models;
+
+namespace PrecisoPRO.Repository
 {
-    public class UsuarioRepository
+    public class UsuarioRepository : IUsuarioRepository
     {
+        //contexto do banco de dados
+        private readonly PrecisoPRODbContext db;
+        public UsuarioRepository(PrecisoPRODbContext context)
+        {
+            db = context;
+        }
+        public async Task<IEnumerable<Usuario>> GetAll()
+        {
+            return await db.Usuarios.ToListAsync();
+        }
+        public bool Adicionar(Usuario club)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Add(Usuario usuario)
+        {
+            db.Add(usuario);
+            return Save();
+        }
+
+      
+
+        public bool Delete(Usuario club)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public Task<Usuario> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> GetByIdAsyncNoTracking(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Usuario>> GetClubByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Usuario club)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
